dd53970479a8e3b102c9f1ce00e6a3061e710b86
Ryan Yeo <ryandc.yeo@gmail.com>
2024-12-06 01:53:38 -0800

    proj4 part 1

--- shardmaster/server.go
+++ shardmaster/server.go
@@ -9,9 +9,18 @@ import (
 	"net"
 	"net/rpc"
 	"os"
+	"sort"
 	"sync"
 	"sync/atomic"
 	"syscall"
+	"time"
+)
+
+const (
+	JOIN  = "Join"
+	LEAVE = "Leave"
+	MOVE  = "Move"
+	QUERY = "Query"
 )
 
 type ShardMaster struct {
@@ -23,32 +32,255 @@ type ShardMaster struct {
 	px         *paxos.Paxos
 
 	configs []Config // indexed by config num
+	done    int
 }
 
 type Op struct {
 	// TODO: Your data here.
+	Type    string
+	GID     int64    // Join, Leave, Move
+	Shard   int      // Move
+	Servers []string // Join
+	Num     int      // Query
+}
+
+func (sm *ShardMaster) wait(seq int) Op {
+	to := 10 * time.Millisecond
+	for {
+		status, value := sm.px.Status(seq)
+		if status == paxos.Decided {
+			return value.(Op)
+		}
+		time.Sleep(to)
+		if to < 10*time.Second {
+			to *= 2
+		}
+	}
+}
+
+func (sm *ShardMaster) propose(op Op) Config {
+	for {
+		seq := sm.done + 1
+		status, value := sm.px.Status(seq)
+
+		if status != paxos.Decided {
+			sm.px.Start(seq, op)
+			op1 := sm.wait(seq)
+			if op1.Type == op.Type && op1.GID == op.GID {
+				cfg := sm.applyOp(op1)
+				sm.done = seq
+				sm.px.Done(seq)
+				return cfg
+			}
+		} else {
+			sm.applyOp(value.(Op))
+			sm.done = seq
+			sm.px.Done(seq)
+		}
+	}
+}
+
+func (sm *ShardMaster) applyOp(op Op) Config {
+	switch op.Type {
+	case JOIN:
+		return sm.applyJoin(op.GID, op.Servers)
+	case LEAVE:
+		return sm.applyLeave(op.GID)
+	case MOVE:
+		return sm.applyMove(op.Shard, op.GID)
+	case QUERY:
+		if op.Num == -1 || op.Num >= len(sm.configs) {
+			return sm.configs[len(sm.configs)-1]
+		}
+		return sm.configs[op.Num]
+	}
+	return Config{}
+}
+
+func (sm *ShardMaster) applyJoin(gid int64, servers []string) Config {
+	lastConfig := sm.configs[len(sm.configs)-1]
+	newConfig := Config{
+		Num:    len(sm.configs),
+		Groups: make(map[int64][]string),
+		Shards: lastConfig.Shards,
+	}
+
+	for gid, servers := range lastConfig.Groups {
+		newConfig.Groups[gid] = append([]string{}, servers...)
+	}
+
+	newConfig.Groups[gid] = append([]string{}, servers...)
+	sm.rebalance(&newConfig)
+	sm.configs = append(sm.configs, newConfig)
+	return newConfig
+}
+
+func (sm *ShardMaster) applyLeave(gid int64) Config {
+	lastConfig := sm.configs[len(sm.configs)-1]
+	newConfig := Config{
+		Num:    len(sm.configs),
+		Groups: make(map[int64][]string),
+		Shards: lastConfig.Shards,
+	}
+
+	for g, servers := range lastConfig.Groups {
+		if g != gid {
+			newConfig.Groups[g] = append([]string{}, servers...)
+		}
+	}
+	for i, g := range newConfig.Shards {
+		if g == gid {
+			newConfig.Shards[i] = 0
+		}
+	}
+	sm.rebalance(&newConfig)
+	sm.configs = append(sm.configs, newConfig)
+	return newConfig
+}
+
+func (sm *ShardMaster) applyMove(shard int, gid int64) Config {
+	lastConfig := sm.configs[len(sm.configs)-1]
+	newConfig := Config{
+		Num:    len(sm.configs),
+		Groups: make(map[int64][]string),
+		Shards: lastConfig.Shards,
+	}
+
+	for g, servers := range lastConfig.Groups {
+		newConfig.Groups[g] = append([]string{}, servers...)
+	}
+
+	newConfig.Shards[shard] = gid
+	sm.configs = append(sm.configs, newConfig)
+	return newConfig
+}
+
+func (sm *ShardMaster) rebalance(config *Config) {
+	if len(config.Groups) == 0 {
+		for i := range config.Shards {
+			config.Shards[i] = 0
+		}
+		return
+	}
+
+	counts := make(map[int64]int)
+	for _, gid := range config.Shards {
+		if gid != 0 {
+			counts[gid]++
+		}
+	}
+	var gids []int64
+	for gid := range config.Groups {
+		gids = append(gids, gid)
+	}
+	sort.Slice(gids, func(i, j int) bool {
+		return gids[i] < gids[j]
+	})
+
+	target := NShards / len(config.Groups)
+	for shard := range config.Shards {
+		if config.Shards[shard] == 0 {
+			var minGid int64
+			minShards := NShards + 1
+
+			for _, gid := range gids {
+				if counts[gid] < minShards {
+					minGid = gid
+					minShards = counts[gid]
+				}
+			}
+
+			config.Shards[shard] = minGid
+			counts[minGid]++
+		}
+	}
+
+	for {
+		var maxGid, minGid int64
+		maxShards := -1
+		minShards := NShards + 1
+		for _, gid := range gids {
+			count := counts[gid]
+			if count > maxShards {
+				maxGid = gid
+				maxShards = count
+			}
+			if count < minShards {
+				minGid = gid
+				minShards = count
+			}
+		}
+
+		if maxShards <= target+1 && minShards >= target {
+			break
+		}
+		for shard := range config.Shards {
+			if config.Shards[shard] == maxGid {
+				config.Shards[shard] = minGid
+				counts[maxGid]--
+				counts[minGid]++
+				break
+			}
+		}
+	}
 }
 
 func (sm *ShardMaster) Join(args *JoinArgs, reply *JoinReply) error {
 	// TODO: Your code here.
+	sm.mu.Lock()
+	defer sm.mu.Unlock()
 
+	op := Op{
+		Type:    JOIN,
+		GID:     args.GID,
+		Servers: args.Servers,
+	}
+	sm.propose(op)
 	return nil
 }
 
 func (sm *ShardMaster) Leave(args *LeaveArgs, reply *LeaveReply) error {
 	// TODO: Your code here.
+	sm.mu.Lock()
+	defer sm.mu.Unlock()
 
+	op := Op{
+		Type: LEAVE,
+		GID:  args.GID,
+	}
+	sm.propose(op)
 	return nil
 }
 
 func (sm *ShardMaster) Move(args *MoveArgs, reply *MoveReply) error {
 	// TODO: Your code here.
+	sm.mu.Lock()
+	defer sm.mu.Unlock()
 
+	op := Op{
+		Type:  MOVE,
+		GID:   args.GID,
+		Shard: args.Shard,
+	}
+	sm.propose(op)
 	return nil
 }
 
 func (sm *ShardMaster) Query(args *QueryArgs, reply *QueryReply) error {
 	// TODO: Your code here.
+	sm.mu.Lock()
+	defer sm.mu.Unlock()
+
+	op := Op{
+		Type: QUERY,
+		Num:  args.Num,
+	}
+
+	if args.Num == -1 || args.Num >= len(sm.configs) {
+		reply.Config = sm.propose(op)
+	} else {
+		reply.Config = sm.configs[args.Num]
+	}
 
 	return nil
 }
